a946a757684b64af566ee2a10d9c1bc5166eaf3a
Sven Reichel <sven.reichel@hotmail.com>
2015-11-04 20:44:43 +0100

    Version 1.9.20

--- app/code/community/Fishpig/AttributeSplash/Model/Layer/Filter/Item.php
+++ app/code/community/Fishpig/AttributeSplash/Model/Layer/Filter/Item.php
@@ -15,22 +15,24 @@ class Fishpig_AttributeSplash_Model_Layer_Filter_Item extends Mage_Catalog_Model
 	*/
 	public function getUrl()
 	{
-		if (!Mage::registry('splash_page')) {
-	    	if (Mage::helper('attributeSplash')->splashPageExists($this->getValue(), Mage::app()->getStore()->getId())) {
-
-    			$splashPage = Mage::getModel('attributeSplash/page')->loadByOptionId($this->getValue());
-
-    			if ($splashPage->canDisplay()) {
-    				$query = array(
-    					$this->getFilter()->getRequestVar() => null,
-    					Mage::getBlockSingleton('page/html_pager')->getPageVarName() => null,
-    				);
-    				
-					return Mage::getUrl('', array('_direct' => $splashPage->getUrlPath(), '_current' => true, '_query' => $query));
-	    		}
+		if (Mage::getStoreConfig('attributeSplash/frontend/inject_links')) {
+			if (!Mage::registry('splash_page')) {
+				if (Mage::helper('attributeSplash')->splashPageExists($this->getValue(), Mage::app()->getStore()->getId())) {
+	
+					$splashPage = Mage::getModel('attributeSplash/page')->loadByOptionId($this->getValue());
+	
+					if ($splashPage->canDisplay()) {
+						$query = array(
+							$this->getFilter()->getRequestVar() => null,
+							Mage::getBlockSingleton('page/html_pager')->getPageVarName() => null,
+						);
+						
+						return Mage::getUrl('', array('_direct' => $splashPage->getUrlPath(), '_current' => true, '_query' => $query));
+					}
+				}
 			}
 		}
-		
+
 		return parent::getUrl();
 	}
 }

ae4a2cc321d597cb527d9a4e693c219c7e8ba318
Sven Reichel <sven.reichel@hotmail.com>
2015-11-04 20:27:12 +0100

    Version 1.9.8

--- app/code/community/Fishpig/AttributeSplash/Model/Mysql4/Page/Collection.php
+++ app/code/community/Fishpig/AttributeSplash/Model/Mysql4/Page/Collection.php
@@ -8,24 +8,38 @@
 
 class Fishpig_AttributeSplash_Model_Mysql4_Page_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract
 {
+	/**
+	 * Flag to determine whether attribute data added
+	 *
+	 * @var bool
+	 */
+	protected $_attributeDataAdded = false
+	
 	public function _construct()
 	{
 		$this->_init('attributeSplash/page');
 	}
 	
+	/**
+	 * Add attribute data to the collection
+	 *
+	 */
 	public function addAttributeOptionData()
 	{
-		$this->getSelect()
-			->join(
-				array('_option_table' => $this->getTable('eav/attribute_option')),
-				"`_option_table`.`option_id` = `main_table`.`option_id`",
-				''
-			)
-			->join(
-				array('_attribute_table' => $this->getTable('eav/attribute')),
-				"`_attribute_table`.`attribute_id` = `_option_table`.`attribute_id`",
-				array('attribute_label' => 'frontend_label', 'attribute_id', 'attribute_code')
-			);
+		if (!$this->_attributeDataAdded) {
+			$this->_attributeDataAdded = true;
+			$this->getSelect()
+				->join(
+					array('_option_table' => $this->getTable('eav/attribute_option')),
+					"`_option_table`.`option_id` = `main_table`.`option_id`",
+					''
+				)
+				->join(
+					array('_attribute_table' => $this->getTable('eav/attribute')),
+					"`_attribute_table`.`attribute_id` = `_option_table`.`attribute_id`",
+					array('attribute_label' => 'frontend_label', 'attribute_id', 'attribute_code')
+				);
+		}
 		
 		return $this;
 	}
@@ -43,6 +57,18 @@ class Fishpig_AttributeSplash_Model_Mysql4_Page_Collection extends Mage_Core_Mod
 				array('store_name' => 'name')
 			);
 			
+		return $this;
+	}
+	
+	/**
+	 * Filter the collection by attribute ID
+	 *
+	 */
+	public function addAttributeCodeFilter($attributeCode)
+	{
+		$this->addAttributeOptionData();
+		$this->addFieldToFilter('attribute_code=?', $attributeCode);
+		
 		return $this;
 	}
 }

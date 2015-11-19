0fcac930cbd0f0fa708d57573ade58fb04a1ec41
Sven Reichel <sven.reichel@hotmail.com>
2015-11-19 12:04:50 +0100

    -Added include_in_menu option for groups

--- app/code/community/Fishpig/AttributeSplash/Block/Adminhtml/Group/Edit/Tab/Design.php
+++ app/code/community/Fishpig/AttributeSplash/Block/Adminhtml/Group/Edit/Tab/Design.php
@@ -17,6 +17,18 @@ class Fishpig_AttributeSplash_Block_Adminhtml_Group_Edit_Tab_Design extends Fish
 	{
 		parent::_prepareForm();
 
+		$fieldset = $this->getForm()->addFieldset('splash_design_menu', array(
+			'legend'=> $this->helper('adminhtml')->__('Menu'),
+			'class' => 'fieldset-wide',
+		));
+		
+		$fieldset->addField('include_in_menu', 'select', array(
+			'name' => 'include_in_menu',
+			'label' => $this->__('Include in Navigation Menu'),
+			'title' => $this->__('Include in Navigation Menu'),
+			'values' => Mage::getModel('adminhtml/system_config_source_yesno')->toOptionArray(),
+		));
+
 		$fieldset = $this->getForm()->addFieldset('splash_design_group_layout', array(
 			'legend'=> $this->helper('adminhtml')->__('Page Layout'),
 			'class' => 'fieldset-wide',
